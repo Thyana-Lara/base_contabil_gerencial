39ee28f683aa52541971d0f07b69bab7d021d7d5
Thyana-Lara <thyana.lara@americasmed.com.br>
2025-12-11 18:11:12 -0300

    nova tabela auxiliar

---
+++ Historico/trusted-criacao_tb_chaves_lancamento_nf_versionadas.sql
@@ -0,0 +1,32 @@
+create table if not exists 
+`sandbox-financeiro-471714.ds_trusted_contabil.tb_chaves_lancamento_nf_versionadas`
+(
+    ano int64,
+    mes int64,
+    chave_nota string,
+    dh_versao datetime,
+    sn_nota_nova string
+);
+
+
+insert into `sandbox-financeiro-471714.ds_trusted_contabil.tb_chaves_lancamento_nf_versionadas`
+(
+    ano,
+    mes,
+    chave_nota,
+    dh_versao,
+    sn_nota_nova
+)
+select
+    extract(year from dt_competencia) as ano,
+    extract(month from dt_competencia) as mes,
+    concat(
+        cast(cd_empresa as string), '-',
+        cast(cd_conta_contabil as string), '-',
+        cd_documento, '-',
+        ds_titulo, '-',
+        cast(sequencia as string)
+    ) as chave_nota,
+    CURRENT_DATE('America/Sao_Paulo') as dh_versao,
+    'nova' as sn_nota_nova
+from `sandbox-financeiro-471714.ds_trusted_contabil.tb_contabil_hist`;
